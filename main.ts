3cb19d3b68ac5483e32cbd07063b36cb1fa61270
hoductrung1211 <hoductrung2604@gmail.com>
2023-05-20 18:32:50 +0700

    Remove all

--- main.ts
+++
@@ -1,9 +0,0 @@
-function deleteRow() {
-    const row = 1;
-
-    console.log(row);
-    console.log(row);
-    console.log(row);
-    console.log(row);
-    console.log(row);
-}
--- test.js
+++
@@ -1,2 +0,0 @@
-let d = new Date(2022, 12);
-console.log(d);
